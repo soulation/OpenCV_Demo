4590a10126930b2040cbed3a7f959023bf09b0f9
Loyale <thanhtt.1393@gmail.com>
2017-04-05 13:14:21 +0700

    [CV_Histogram] Add function for producing lomography effect
    - Add void lomography()

--- CV_Histogram/Histogram/main.cpp
+++ CV_Histogram/Histogram/main.cpp
@@ -9,6 +9,7 @@ using namespace cv;
 
 void drawHistogram(Mat img);
 void equalize(Mat img);
+void lomography(Mat img);
 
 int main(int argc, const char** argv)
 {
@@ -18,7 +19,8 @@ int main(int argc, const char** argv)
 
     imshow("Castle", castle);
     drawHistogram(castle);
-    equalize(castle);
+    //equalize(castle);
+    lomography(castle);
 
     waitKey(0);
     return 1;
@@ -99,4 +101,42 @@ void equalize(Mat img)
     // Show image
     imshow("Equalized", result);
 
+}
+void lomography(Mat img)
+{
+    Mat result;
+    const float exponential_e = std::exp(1.0);
+    // Create Lookup table for color curve effect
+    Mat lut(1, 256, CV_8UC1);
+    for (int i = 0; i < 256; i++)
+    {
+        float x = (float)i / 256.0;
+        lut.at<uchar>(i) = cvRound(256 * (1 / (1 + pow(exponential_e,-((x - 0.5) / 0.1)))));
+    }
+
+    // Split the image channels and apply curve transform only to red channel
+    vector<Mat> bgr;
+    split(img, bgr);
+    LUT(bgr[2], lut, bgr[2]);
+    // merge result
+    merge(bgr, result);
+
+    // Create image for halo dark
+    Mat halo(img.rows, img.cols, CV_32FC3, Scalar(0.3, 0.3, 0.3));
+    // Create circle
+    circle(halo, Point(img.cols / 2, img.rows / 2), img.cols / 3, Scalar(1, 1, 1), -1);
+    blur(halo, halo, Size(img.cols / 3, img.cols / 3));
+
+    // Convert the result to float to allow multiply by 1 factor
+    Mat resultf;
+    result.convertTo(resultf, CV_32FC3);
+
+    // Multiply result with halo
+    multiply(resultf, halo, resultf);
+
+    // Convert to 8 bits
+    resultf.convertTo(result, CV_8UC3);
+
+    // show result
+    imshow("Lomograpy", result);
 }
